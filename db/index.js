86bf1fec9cbd12776dca6a07da428557fd9b0d23
Saul Calleja <scalleja12345@gmail.com>
2022-05-29 13:27:32 -0400

    Empty Git

--- db/index.js
+++ db/index.js
@@ -5,7 +5,7 @@ let MONGODB_URI = process.env.MONGODB_CONNECTION_STRING;
 mongoose
   .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => {
-    console.log("You're Connected Successfully To MongoDB");
+    console.log("You're Connected Successfully To MongoDB!");
   })
   .catch((e) => {
     console.error("Connection error has occurred", e.message);
